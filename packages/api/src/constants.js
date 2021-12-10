c5cc522a339855867c87d48358f206591da458a9
redaphid <iam@aaronherres.com>
2021-12-09 20:04:22 -0700

    making everything go through env

--- packages/api/src/constants.js
+++ packages/api/src/constants.js
@@ -1,4 +1,32 @@
-// let MAGIC_SECRET_KEY, SALT, PINATA_JWT, SENTRY_DSN, DATABASE_TOKEN, CLUSTER_SERVICE, LOGTAIL_TOKEN, MAILCHIMP_API_KEY, METAPLEX_AUTH_TOKEN
+import { config } from 'dotenv'
+
+config({ debug: true })
+
+const {
+  MAGIC_SECRET_KEY,
+  SALT,
+  SENTRY_DSN,
+  DATABASE_TOKEN,
+  CLUSTER_SERVICE,
+  LOGTAIL_TOKEN,
+  MAILCHIMP_API_KEY,
+  CLUSTER_API_URL,
+  DATABASE_URL,
+  DEBUG,
+  ENV,
+} = process.env
+
+if (!SALT) {
+  throw new Error('SALT is not defined')
+}
+
+if (!DATABASE_TOKEN) {
+  throw new Error('DATABASE_TOKEN is not defined')
+}
+
+if (!LOGTAIL_TOKEN) {
+  throw new Error('LOGTAIL_TOKEN is not defined')
+}
 
 export const secrets = {
   salt: SALT,
@@ -14,6 +42,7 @@ const CLUSTER1 = 'https://nft.storage.ipfscluster.io/api/'
 const CLUSTER2 = 'https://nft2.storage.ipfscluster.io/api/'
 let clusterUrl
 
+console.log('ENVIRONMENT IS', process.env)
 switch (CLUSTER_SERVICE) {
   case 'IpfsCluster':
     clusterUrl = CLUSTER1
@@ -24,6 +53,10 @@ switch (CLUSTER_SERVICE) {
   default:
     clusterUrl = CLUSTER_API_URL
     break
+}
+
+if (!clusterUrl) {
+  throw new Error('CLUSTER_API_URL or CLUSTER_SERVICE must be set')
 }
 
 export const cluster = {
@@ -41,8 +74,15 @@ export const cluster = {
   localAddThreshold: 0,
 }
 
+if (!DATABASE_URL) {
+  throw new Error('DATABASE_URL must be set')
+}
+
 export const database = {
   url: DATABASE_URL,
 }
+
+export const environment = ENV
+export const version = '0.0.1'
 
 export const isDebug = DEBUG === 'true'
